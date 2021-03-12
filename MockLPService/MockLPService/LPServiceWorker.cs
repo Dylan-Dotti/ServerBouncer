09ea3b5fb5c396362cdf58625adde7c3c4a56f06
Dylan-Dotti <invocation42@yahoo.com>
2021-03-11 21:19:50 -0500

    Added MockLPService

---
+++ MockLPService/MockLPService/LPServiceWorker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TopshelfBoilerplate;
+
+namespace MockLPService
+{
+    class LPServiceWorker : IServiceWorker
+    {
+        public void Start()
+        {
+            Console.WriteLine("LP service started");
+        }
+
+        public void Stop()
+        {
+            Console.WriteLine("LP service stopped");
+        }
+    }
+}
---
+++ MockLPService/MockLPService/Program.cs
@@ -0,0 +1,16 @@
+﻿using TopshelfBoilerplate;
+
+namespace MockLPService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new TopshelfService(
+                "SWISSLOG_MOCK_LP_SERVICE",
+                "SWISSLOG_MOCK_LP_SERVICE",
+                "A mock LP service for testing")
+                .Run(new LPServiceWorker());
+        }
+    }
+}
